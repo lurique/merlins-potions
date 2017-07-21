23c68e975d5946fb733ab6a0c4040fb64f386ba6
root <lucashenrique.he@gmail.com>
2017-07-21 17:21:15 -0300

    Updating project

---
+++ source/js/potions.js
@@ -0,0 +1,32 @@
+var App = App || {};
+
+;(function() {
+
+	App.Potions = {
+
+		init: function() {
+			this.cacheSelectors();
+			this.bindEvents();
+		},
+
+		cacheSelectors: function() {
+
+		},
+
+		bindEvents: function() {
+
+		},
+
+		Events: {
+			callProducts: function() {
+
+			},
+
+			callProductDescription: function() {
+
+			}
+		}
+
+	}
+
+})();
